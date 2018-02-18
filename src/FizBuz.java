cbb4492dd487474fe9564b0417635a6453d21934
meeliana <javabcamp@gmail.com>
2018-02-17 23:37:15 -0600

    Update FizBuz.java

--- src/FizBuz.java
+++ src/FizBuz.java
@@ -9,7 +9,7 @@ public static void main(String[] args){
 	if(i % 3 ==0 && i %5 == 0){
 		
 		System.out.println( "FizzBuzz");}
-	
+	test
 		else if (i % 3 ==0){System.out.println( "Fizz");}
 		else if (i %5 == 0){System.out.println( "Buzz");}
 		else{System.out.println(i);}
